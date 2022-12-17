958bb7762edbff95196a45f69993b7aed458245a
Ariscott72 <davidkoutouandjiragbou@gmail.com>
2022-12-17 00:01:40 +0000

    derniere retouche

--- insp.php
+++ insp.php
@@ -12,17 +12,22 @@
             }catch(PDOException $e){
                 die('Erreur : '.$e->getMessage());
 
-
+                
             }
 
             if (isset($_POST['btn'])){
 
-                if (isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['tel'])){
+                if (isset($_POST['nom']) && isset($_POST['prenoms']) && isset($_POST['genre']) && isset($_POST['inst']) && isset($_POST['prof']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['ville'])){
                      $insertion=$newdb->prepare('INSERT INTO inscript VALUES(NULL
                     ,:nom,:mail,:tel)');
                     $insertion->bindValue(':nom',$_POST['nom']);
+                    $insertion->bindValue(':prenoms',$_POST['prenoms']);
+                    $insertion->bindValue(':genre',$_POST['genre']);
+                    $insertion->bindValue(':inst',$_POST['inst']);
+                    $insertion->bindValue(':prof',$_POST['prof']);
                     $insertion->bindValue(':mail',$_POST['email']);
                     $insertion->bindValue(':tel',$_POST['tel']);
+                    $insertion->bindValue(':ville',$_POST['ville']);
                     $verification=$insertion->execute();
                     if ($verification){
 
